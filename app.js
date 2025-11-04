b4d5764fa387b2b9234ae0d5ca1206b090d743fe
Akaiokaa <161771111+Akaiokaa@users.noreply.github.com>
2025-11-04 11:42:23 -0800

    Created a new variable person to store the json information.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const PORT = 3015
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.render('home', {name: 'Donald'});
+    const person = {name: 'Donald', occupation: 'plumber'}
+    res.render('home', {person});
 })
 app.listen('/', (req, res) => {
     app.listen(PORT, () => console.log(`Server is running on localhost http://localhost:${PORT}`))
