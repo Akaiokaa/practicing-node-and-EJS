90d82229b4e6e507cef77ec446bbbdd050335a57
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2025-11-04 11:36:56 -0800

    Finished testing for primary

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import express from 'express'
 
 const app = express();
 
-const PORT = 3012
+const PORT = 3015
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', {name: 'Donald'});
 })
 app.listen('/', (req, res) => {
     app.listen(PORT, () => console.log(`Server is running on localhost http://localhost:${PORT}`))
